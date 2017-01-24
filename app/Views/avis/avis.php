eeb071321d7acb293db5b007ebb14f1e869d1ca0
SailorMood <bastienthefly@live.fr>
2017-01-24 01:09:15 +0100

    Tout changer, pas marcher

--- app/Views/avis/avis.php
+++ app/Views/avis/avis.php
@@ -1,23 +1,8 @@
-<?php use Controller\AvisController;
-
-$this->layout('layout', ['title' => 'Avis']) ?>
+<?php $this->layout('layout', ['title' => 'Avis']) ?>
 
 <?php $this->start('main_content') ?>
 
-<div id="formAvis" class="hide">
-    <h2>Votre avis nous intéresse</h2>
-    <form class="" action="avis.php" method="post">
-      <label class="inf">&#9998;Informations :</label><br>
-      <input type="text" required="required" name="name" value="" placeholder="Nom*"><br>
-      <input type="text" required="required" name="firstname" value="" placeholder="Prénom*"><br>
-      <input type="text" required="required" name="numberphone" value="" placeholder="Téléphone*"><br>
-      <input type="email" required="required" name="email" value="" placeholder="Email*"><br>
-      <textarea required="required" name="message" placeholder="Message*"></textarea><br>
-      <input type="hidden" name="users_id" value="">
-      <input class="" type="submit" name="addMessage" value="Envoyer">
-    </form>
-    <p>*Champs devant obligatoirement être renseignés</p>
-  </div>
+<?php include('poster.php'); ?>
   <div class="customReview">
       <?php \Controller\AvisController::avisUtilisateurs(); ?>
   </div>
